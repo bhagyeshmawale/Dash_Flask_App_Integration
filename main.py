0ae94750f95eb2135af0631aa7ba997423aa5b0c
bhagyeshmawale <bhagyeshmawale123@gmail.com>
2021-07-25 12:22:53 +0530

    first commit

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask,render_template,request
 from datetime import datetime
 import json
 
+from maindash import create_dash_application
 
 
 with open('config.json','r') as c:
@@ -11,11 +12,11 @@ local_Server = True
 
 app = Flask (__name__)
 
-
+create_dash_application(app)
 
 @app.route("/")
 def about():
 	return render_template('about.html',params = params)
 
 
-app.run(debug=True,port=8004)
+app.run(debug=True,port=8005)
